08bd28b7ee123f21f7d26a6462072062726cef8f
Aleph Melo <alephmelo@icloud.com>
2016-02-06 03:17:08 -0300

    Apply some initial PEP8

--- Adaline.py
+++ Adaline.py
@@ -9,14 +9,14 @@
 # man = [-1,-1,-1,1] | answer = -1
 # To-do: Apply PEP8
 # (sinapses)
-w = [0,0,0,0]
+w = [0, 0, 0, 0]
 # input
-x = [[-1,-1,1,1],
-     [1,1,1,1],
-     [1,1,-1,1],
-     [-1,-1,-1,1]]
+x = [[-1, -1, 1, 1],
+     [1, 1, 1, 1],
+     [1, 1, -1, 1],
+     [-1, -1, -1, 1]]
 # expected answers
-t = [1,1,1,-1]
+t = [1, 1, 1, -1]
 # bias (ajuste fino)
 b = 0
 # output
@@ -34,69 +34,70 @@ animal = ""
 resposta = ""
 
 # data dict
-d = {'-1,-1,1,1' : 'cao', 
-     '1,1,1,1' : 'gato', 
-     '1,1,-1,1' : 'cavalo',
-     '-1,-1,-1,1' : 'homem' }
+d = {'-1,-1,1,1': 'cao',
+     '1,1,1,1': 'gato',
+     '1,1,-1,1': 'cavalo',
+     '-1,-1,-1,1': 'homem'}
 
 print("Treinando")
 
 # convert list to string function
+
+
 def listToString(list):
     s = str(list).strip('[]')
     s = s.replace(' ', '')
     return s
 
 # magic happens
-for k in range(1,max_int):
-    acertos = 0    
-    print("INTERACAO "+str(k)+"-------------------------")
-    for i in range(0,len(x)):
+for k in range(1, max_int):
+    acertos = 0
+    print("INTERACAO " + str(k) + "-------------------------")
+    for i in range(0, len(x)):
         soma = 0
-        
+
         # get animal's name from dict
         if d.has_key(listToString(x[i])):
-            animal = d[listToString(x[i])]  
+            animal = d[listToString(x[i])]
         else:
             animal = ""
-            
+
         # para calcular a saida do adaline, cada entrada de x eh multiplicada
         # pelo seu peso w correspondente
-        for j in range(0,len(x[i])):
+        for j in range(0, len(x[i])):
             soma += x[i][j] * w[j]
 
         # the output is eaual the previous one
         y_in = soma
-        #print("y_in = ",str(y_in))        
-                  
+        #print("y_in = ",str(y_in))
 
         # activate function
         if y_in >= 0:
-            y = 1       
+            y = 1
         else:
             y = -1
-            
+
         # update
-        for j in range (0,len(w)):                
-           # regra delta
-           w[j] = w[j] + taxa_aprendizado * (t[i] - y_in) * x[i][j]        
-        
+        for j in range(0, len(w)):
+            # regra delta
+            w[j] = w[j] + taxa_aprendizado * (t[i] - y_in) * x[i][j]
+
         if y == t[i]:
-            acertos+=1
+            acertos += 1
             resposta = "acerto"
         else:
             resposta = "erro"
-        
+
         # print the aswer
         if y == 1:
-            print(animal+" = quadrupede = "+resposta)
+            print(animal + " = quadrupede = " + resposta)
         elif y == 0:
-            print(animal+" = padrao nao identificado = "+resposta)
+            print(animal + " = padrao nao identificado = " + resposta)
         elif y == -1:
-            print(animal+" = bipede = "+resposta)
-            
+            print(animal + " = bipede = " + resposta)
+
     if acertos == len(x):
-        print("Funcionalidade aprendida com "+str(k)+" interacoes")
+        print("Funcionalidade aprendida com " + str(k) + " interacoes")
         break
     print("")
 print("Finished")
--- Perceptron.py
+++ Perceptron.py
@@ -11,14 +11,14 @@
 # man = [-1,-1,-1,1] | answer = -1
 
 # pesos (sinapses)
-w = [0,0,0,0]
+w = [0, 0, 0, 0]
 # input
-x = [[-1,-1,1,1],
-     [1,1,1,1],
-     [1,1,-1,1],
-     [-1,-1,-1,1]]
+x = [[-1, -1, 1, 1],
+     [1, 1, 1, 1],
+     [1, 1, -1, 1],
+     [-1, -1, -1, 1]]
 # expected answers
-t = [1,1,1,-1]
+t = [1, 1, 1, -1]
 # bias (adjustment)
 b = 0
 # output
@@ -38,40 +38,43 @@ animal = ""
 resposta = ""
 
 # data dictionary
-d = {'-1,-1,1,1' : 'cao',
-     '1,1,1,1' : 'gato',
-     '1,1,-1,1' : 'cavalo',
-     '-1,-1,-1,1' : 'homem' }
+d = {'-1,-1,1,1': 'cao',
+     '1,1,1,1': 'gato',
+     '1,1,-1,1': 'cavalo',
+     '-1,-1,-1,1': 'homem'}
 
 print("Treinando")
 
 # list to string function
+
+
 def listToString(list):
     s = str(list).strip('[]')
     s = s.replace(' ', '')
     return s
 
 # begin
-for k in range(1,max_int):
-    acertos = 0    
-    print("INTERACAO "+str(k)+"-------------------------")
-    for i in range(0,len(x)):
+for k in range(1, max_int):
+    acertos = 0
+    print("INTERACAO " + str(k) + "-------------------------")
+    for i in range(0, len(x)):
         soma = 0
 
         # get animal's name inside the dict
         if d.has_key(listToString(x[i])):
-            animal = d[listToString(x[i])]  
+            animal = d[listToString(x[i])]
         else:
             animal = ""
 
-        # para calcular a saida do perceptron, cada entrada de x eh multiplicada
+        # para calcular a saida do perceptron, 
+        # cada entrada de x eh multiplicada
         # pelo seu peso w correspondente
-        for j in range(0,len(x[i])):
+        for j in range(0, len(x[i])):
             soma += x[i][j] * w[j]
 
         # a saida eh igual a adicao do bias com a soma anterior
         y_in = b + soma
-        #print("y_in = ",str(y_in))
+        # print("y_in = ",str(y_in))
 
         # funcao de saida eh determinada pelo threshold
         if y_in > threshold:
@@ -79,28 +82,28 @@ for k in range(1,max_int):
         elif y_in >= -threshold and y_in <= threshold:
             y = 0
         else:
-            y = -1        
+            y = -1
 
         # atualiza os pesos caso a saida nao corresponda ao valor esperado
         if y == t[i]:
-            acertos+=1
+            acertos += 1
             resposta = "acerto"
         else:
-            for j in range (0,len(w)):                
+            for j in range(0, len(w)):
                 w[j] = w[j] + (taxa_aprendizado * t[i] * x[i][j])
             b = b + taxa_aprendizado * t[i]
             resposta = "Falha - Peso atualizado"
 
-        #imprime a resposta
+        # imprime a resposta
         if y == 1:
-            print(animal+" = quadrupede = "+resposta)
+            print(animal + " = quadrupede = " + resposta)
         elif y == 0:
-            print(animal+" = padrao nao identificado = "+resposta)
+            print(animal + " = padrao nao identificado = " + resposta)
         elif y == -1:
-            print(animal+" = bipede = "+resposta)
+            print(animal + " = bipede = " + resposta)
 
     if acertos == len(x):
-        print("Funcionalidade aprendida com "+str(k)+" interacoes")
-        break;
+        print("Funcionalidade aprendida com " + str(k) + " interacoes")
+        break
     print("")
 print("Finalizado")
